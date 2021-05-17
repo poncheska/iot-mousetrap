9e163c701eea123ba6558f708b37eb2f9aa7feca
kseniiaguk <st068928@student.spbu.ru>
2021-05-17 23:42:29 +0300

    update signIn

--- front/script.js
+++ front/script.js
@@ -29,11 +29,12 @@ async function signIn(form) {
         // let status = join(formdata);
         if (!status){
             if (!document.querySelector("#join-error")){
+                // alert(JSON.parse(joinResponse).message);
                 let errorMessage = document.createElement('div');
                 errorMessage.id = "join-error";
                 errorMessage.className = "error";
                 // errorMessage.innerHTML = "<strong>fdghjk</strong>";
-                errorMessage.innerHTML = `<strong>${JSON.parse(joinResponse).message}</strong>`;
+                errorMessage.innerHTML = `<strong>${JSON.parse(joinResponse.json()).message}</strong>`;
                 document.querySelector("#form-name-join").after(errorMessage);
             }
             return false;
