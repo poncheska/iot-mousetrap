f5bf6216e6094798f6d88a8714646ff38692640b
poncheska <43468296+poncheska@users.noreply.github.com>
2021-05-19 19:12:52 +0300

    Update script.js

--- front/script.js
+++ front/script.js
@@ -174,5 +174,5 @@ document.forms.signIn.addEventListener("submit", function(event) {
 	signIn(document.forms.signIn);
     event.currentTarget.submit();
 });
+savingChanges();
 document.querySelector("#update-button").addEventListener("click", update);
-savingChanges()
